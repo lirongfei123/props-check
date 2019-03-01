d8d47d273826e3c97534a4ba2f5ca94c9b3096d6
rongping.lrp@alibaba-inc.com <rongping.lrp@alibaba-inc.com>
2019-03-01 09:42:48 +0800

    init

---
+++ checkPropTypes.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+var printWarning = function() {};
+
+/**
+ * Assert that the values match with the type specs.
+ * Error messages are memorized and will only be shown once.
+ *
+ * @param {object} typeSpecs Map of name to a ReactPropType
+ * @param {object} values Runtime values that need to be type-checked
+ * @param {string} location e.g. "prop", "context", "child context"
+ * @param {string} componentName Name of the component for error messages.
+ * @param {?Function} getStack Returns the component stack.
+ * @private
+ */
+function checkPropTypes(typeSpecs, values, location, componentName, getStack) {
+  const errorTypes = [];
+  const loggedTypeFailures = {};
+  for (var typeSpecName in typeSpecs) {
+    if (typeSpecs.hasOwnProperty(typeSpecName)) {
+      var error;
+      // Prop type validation may throw. In case they do, we don't want to
+      // fail the render phase where it didn't fail before. So we log it.
+      // After these have been cleaned up, we'll let them throw.
+      try {
+        // This is intentionally an invariant that gets caught. It's the same
+        // behavior as without this statement except with a better message.
+        if (typeof typeSpecs[typeSpecName] !== 'function') {
+          var err = Error(
+            (componentName || 'React class') + ': ' + location + ' type `' + typeSpecName + '` is invalid; ' +
+            'it must be a function, usually from the `prop-types` package, but received `' + typeof typeSpecs[typeSpecName] + '`.'
+          );
+          err.name = 'Invariant Violation';
+          throw err;
+        }
+        error = typeSpecs[typeSpecName](values, typeSpecName, componentName, location, null);
+      } catch (ex) {
+        error = ex;
+      }
+      if (error instanceof Error && !(error.message in loggedTypeFailures)) {
+        // Only monitor this failure once because there tends to be a lot of the
+        // same error.
+        loggedTypeFailures[error.message] = true;
+
+        var stack = getStack ? getStack() : '';
+        errorTypes.push({
+          name: typeSpecName,
+          message: error.message,
+        });
+        printWarning(
+          'Failed ' + location + ' type: ' + error.message + (stack != null ? stack : '')
+        );
+      }
+    }
+    return errorTypes;
+  }
+}
+
+module.exports = checkPropTypes;
